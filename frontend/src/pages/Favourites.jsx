a14bdb03120ddfdfba38369a1a9f7bc648931d0d
LogaGill489 <logagill489@ed.amdsb.ca>
2025-04-15 18:11:13 -0400

    Added a navbar component + used react-router to help with them.

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -1,8 +1,8 @@
-function  Favorite() {
+function  Favorites() {
     return <div className="favorites-empty">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies and they will appear here.</p>
     </div>
 }
 
-export default Favorite;
+export default Favorites;
